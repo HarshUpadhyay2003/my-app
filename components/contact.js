91b2d20d5bc317535225687e9a3b685523fc42d2
Harsh Upadhyay <125578074+HarshUpadhyay2003@users.noreply.github.com>
2024-07-21 12:13:38 +0530

    content updates.

--- components/contact.js
+++ components/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     "https://docs.google.com/forms/d/1-qNruPTAtIscj4K2XreCG7_DgUr7PNbp7xxE1oEl_1s/formResponse";
   return (
     <>
-      <div className="contact-container md:divide-x-2 divide-black divide-solid caudex-regular flex flex-col md:flex-row">
+      <div className="contact-container md:divide-x-2 divide-black divide-solid caudex-regular mt-64 flex flex-col md:flex-row">
         <div className="p-5 md:pl-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex-1">
           <h1 className="text-xl md:text-3xl">AstraNex Services Pvt Ltd.</h1>
           <br />
